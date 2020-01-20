18c0df4a864435000efa190ca9dea60eeddb18d0
Davide Cavaliere <cavaliere.davide@gmail.com>
2020-01-20 10:59:05 +0100

    feat(apigator): @Endpoint sets default LambdaHadler and provides option to override it

--- packages/apigator/src/lib/endpoint/endpoint.decorator.ts
+++ packages/apigator/src/lib/endpoint/endpoint.decorator.ts
@@ -1,4 +1,7 @@
 import 'reflect-metadata';
+import { setImplementation } from '@microgamma/digator';
+import { LambdaHandler } from '../lambda/handler/lambda-handler';
+import { AwsEventHandler } from '../lambda/handler/aws-event-handler';
 
 const EndpointMetadata = Symbol.for('Endpoint');
 
@@ -7,9 +10,20 @@ export interface EndpointOptions {
   readonly basePath?: string;
   readonly private?: boolean;
   readonly cors?: boolean;
+  readonly providers?: Array<{provide: any, implementation: any}>;
 }
 
 export function Endpoint(options: EndpointOptions): ClassDecorator {
+  // Set default Handlers, if user provides a different implementation it will overwritten below
+  // defaults to AwsLambdaHandler
+  setImplementation({
+    provide: LambdaHandler,
+    implementation: AwsEventHandler
+  });
+
+  if (options.providers) {
+    options.providers.forEach(setImplementation);
+  }
 
   return <TFunction extends Function>(target: TFunction) => {
 
