575d3b09c6ad35d838d5a5ee988a502405b9bea5
Davide Cavaliere <cavaliere.davide@gmail.com>
2020-01-23 10:03:10 +0100

    docs(apigator): adding documentation

--- packages/apigator/README.md
+++ packages/apigator/README.md
@@ -1,35 +1,6 @@
 # @microgamma/apigator [![npm version](https://badge.fury.io/js/%40microgamma%2Fapigator.svg)](https://badge.fury.io/js/%40microgamma%2Fapigator)
 
-This project is mean to make more elegant api endpoint using typescript classes and decorators.
-
-## What is does
-
-- extracts variables from `event`. Doesn't matter if they're in the body or in the header or path: just put the variable's name you wan to retrieve as an argument of the function.
-- bear in mind that the option object passed to the @Lambda decorator do not have any effect if you don't use the serverless framework.
+This package contains a set up typescripts decorators that help you build apis in form of classes.
 
 
-## How to use
-`npm i -S @microgamma/apigator`
-
-```
-// handler.ts
-
-export class MyService {
-
-  @Lambda({
-    name: 'findAll',
-    path: '/{name}',
-    method: 'GET'
-  })
-  public async hello(name) {
-    return `Hello ${name}`;
-  }
-}
-
-module.exports = new MyService();
-
-// in this case handler would be handler.hello
-```
-
-compile your ts file and upload it to aws.
 
--- packages/apigator/src/lib/endpoint/endpoint.decorator.ts
+++ packages/apigator/src/lib/endpoint/endpoint.decorator.ts
@@ -4,6 +4,13 @@ import { setInjectable } from '@microgamma/digator';
 
 const EndpointMetadata = Symbol.for('Endpoint');
 
+/**
+ * readonly name: string;  // name of the endpoint
+ * readonly basePath?: string;  // base path that will be prepended to the path defined in each lambda.
+ * readonly private?: boolean;  // Default: false. Setting this field is equivalent to add its value to every @Lambda. If any @Lambda as private set then that will have precedence
+ * readonly cors?: boolean;  // Default: false. Same as private.
+ * readonly providers?: Array<{provide: any, implementation: any}>;  // Use the provided implementation instead of the default.
+ */
 export interface EndpointOptions {
   readonly name: string;
   readonly basePath?: string;
@@ -12,6 +19,27 @@ export interface EndpointOptions {
   readonly providers?: Array<{provide: any, implementation: any}>;
 }
 
+/**
+ * Define an Endpoint decorating a class such as
+ * ```typescript
+ *
+ * @Endpoint({
+ *   name: 'my-endpoint',
+ *   basePath: '/test', // Optional base path, Defaults to /
+ *   private: false, // Optional, Defaults to false
+ *   cors: true, // Optional, Defaults to false,
+ *   providers: [{
+ *     provide: LambdaDefaultHandler,
+ *     implementation: MyCustomLambdaHandler
+ *   }] // List of providers that will use a different implementation at runtime
+ * })
+ * export class MyEndpoint {}
+ *
+ * ```
+ *
+ * @param options EndpointOptions
+ * @constructor
+ */
 export function Endpoint(options: EndpointOptions): ClassDecorator {
   // Set default Handlers, if user provides a different implementation it will overwritten below
   // defaults to AwsLambdaHandler
@@ -31,11 +59,21 @@ export function Endpoint(options: EndpointOptions): ClassDecorator {
   };
 }
 
-export function getEndpointMetadata(instance) {
+/**
+ * Get EndpointOptions stored on a decorated class from its instance
+ * @param instance - the instance of the decorated class
+ * @return EndpointOptions
+ */
+export function getEndpointMetadata(instance): EndpointOptions {
   return Reflect.getMetadata(EndpointMetadata, instance.constructor);
 }
 
-export function getEndpointMetadataFromClass(klass) {
+/**
+ * Get EndpointOptions stored on a decorated class
+ * @param klass - the class
+ * @return EndpointOptions
+ */
+export function getEndpointMetadataFromClass(klass): EndpointOptions {
   return Reflect.getMetadata(EndpointMetadata, klass);
 }
 
